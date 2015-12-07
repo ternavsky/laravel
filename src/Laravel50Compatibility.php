6c2678747ec04774c6f973931eca41176b53f965
ternavsky <e_ternavsky@mail.ru>
2015-12-07 12:41:22 +0300

    Update Laravel50Compatibility.php
    To fix error 
    [ErrorException]
    
    Declaration of BackupManager\Laravel\Laravel50Compatibility::table() should be compatible with Illuminate\Console\Command::table(array $headers, array $rows, $style = 'default')

--- src/Laravel50Compatibility.php
+++ src/Laravel50Compatibility.php
@@ -11,7 +11,7 @@ trait Laravel50Compatibility {
      * @internal param string $style
      * @return void
      */
-    public function table($headers, $rows, $style = 'default') {
+    public function table(array $headers, array $rows, $style = 'default') {
         $table = $this->getHelperSet()->get('table');
         $table->setHeaders($headers);
         $table->setRows($rows);
